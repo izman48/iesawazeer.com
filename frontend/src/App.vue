792c9abacb35659c60c30beff7ae8fbbf5714f76
Iesa Wazeer <iesa.wazeer7@gmail.com>
2020-07-23 00:34:40 +0530

    changed ui and added vue router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,38 +1,79 @@
 <template>
   <v-app>
-    
-    <NavBar />
-
-      <!-- Sizes your content based upon application components -->
-    <v-main>
-
-      <!-- Provides the application the proper gutter -->
-      <v-container fluid app>
-        <HelloWorld />
-        <!-- If using vue-router -->
-        <!-- <router-view></router-view> -->
-      </v-container>
-    </v-main>
+    <v-app-bar app color="primary" dark>
+      <v-avatar > 
+        <img
+        src='./assets/iesa.jpg'
+        alt="Iesa"
+        >
+      </v-avatar>
+			<v-spacer></v-spacer>
+      <v-toolbar-title >Iesa Wazeer</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn
+				v-for="link in links"
+				:key="`${link.label}-header-link`"
+				text
+				rounded
+				:to="link.url"
+			>
+				{{ link.label }}
+			</v-btn>
+		</v-app-bar>
+		<v-content>
+			<router-view></router-view>
+		</v-content>
+		<v-footer color="primary lighten-1" padless>
+			<v-layout justify-center wrap>
+				<v-btn
+					v-for="link in links"
+					:key="`${link.label}-footer-link`"
+					color="white"
+					text
+					rounded
+					class="my-2"
+					:to="link.url"
+				>
+					{{ link.label }}
+				</v-btn>
+				<v-flex primary lighten-2 py-4 text-center white--text xs12>
+					{{ new Date().getFullYear() }} — <strong>Iesa Wazeer Copyright &copy;</strong>
+				</v-flex>
+			</v-layout>
+		</v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-import NavBar from './components/NavBar';
-// import Card from './components/Card';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-    NavBar,
-    // Card,
-  },
-
-  data: () => ({
-    
-  //
-  }),
-};
+	name: 'App',
+	data() {
+		return {
+			links: [
+				{
+					label: 'Home',
+					url: '/'
+				},
+				{
+					label: 'About',
+					url: '/about'
+        },
+        {
+					label: 'Projects',
+					url: '/projects'
+				},
+      ],
+      contact: [
+        {
+          github: 'GitHub',
+          url: 'https://github.com/izman48'
+        },
+        {
+          linkedin: 'LinkedIn',
+          url: 'https://www.linkedin.com/in/iesaw/'
+        },
+      ]
+		}
+	}
+}
 </script>
