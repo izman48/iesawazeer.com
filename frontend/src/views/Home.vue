7ec3d6095711bc64548001e49de0fa849108933b
Iesa <IesaW@users.noreply.github.com>
2020-07-23 00:34:40 +0530

    changed ui and added vue router

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi magni aliquid, similique quibusdam temporibus in amet blanditiis architecto ea deserunt cum enim itaque nihil deleniti minima laborum dolorum placeat veniam.
+    </p>    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
