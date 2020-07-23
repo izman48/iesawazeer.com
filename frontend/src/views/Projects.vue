6ce5f618ad6cee30d814d7c8c12f7afffa8ecf37
Iesa Wazeer <iesa.wazeer7@gmail.com>
2020-07-24 03:48:37 +0530

    created dynamic layout for projects, name on header is a button and ui changes

--- frontend/src/views/Projects.vue
+++ frontend/src/views/Projects.vue
@@ -1,18 +1,84 @@
 <template>
   <div class="projects">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <HelloWorld />
+    
+    <v-main
+    >
+      <v-row>
+        <v-col 
+          cols="12"
+          sm="6"
+          md="4"
+          lg="4"
+          v-for="(data,index) in info"
+          :key="index"
+          
+        >
+          <Card 
+            :title="data.title"
+            :tags="data.tags"
+            :img="data.img"
+            :text="data.text"
+          />
+        </v-col>
+      </v-row>
+
+    </v-main>
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card.vue'
 
 export default {
   name: 'Projects',
+  data () {
+      return {
+        info: [
+          {
+            img:'Traffic.png',
+            title:'Visualising Traffic using Automated Agnets',
+            tags: ['Java','Processing','AI'],
+            text: 'We made cars go around a roundabout using automated agents',
+            // index: 1
+          },
+          {
+            img:'DocumentOCR.jpg',
+            title:'Document detection and optimisation using Image Processing',
+            tags: ['Python', 'OpenCV', 'Tesseract'],
+            text: 'We use Python and OpenCV to detect documents and then optimise them for text extraction ',
+            // index: 2
+
+          },
+          {
+            img:'Website.png',
+            title:'Derivitive Trade Website',
+            tags: ['Javascript','Python','Django','MongoDB'],
+            text: 'We created a website which accepts trades and stores them in a database. Using this we predict Derivitve trade info',
+            // index: 3
+
+          },
+          {
+            img:'Website.png',
+            title:'Derivitive Trade Website',
+            tags: ['Javascript','Python','Django','MongoDB'],
+            text: 'We created a website which accepts trades and stores them in a database. Using this we predict Derivitve trade info',
+            // index: 4
+
+          },
+        ]        
+      }
+    },
+    computed: {
+      numRows() {
+        console.log(Math.ceil(this.info.length/2));
+        return Math.ceil(this.info.length/2)
+      }
+    },
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
