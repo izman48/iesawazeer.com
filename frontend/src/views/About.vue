35c227b49a432211c7310b192b7eb1fe1cc48b55
Iesa Wazeer <iesa.wazeer7@gmail.com>
2020-07-26 18:43:35 +0530

    added search bar and filled in about page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-img src="../assets/iesa-about.jpg" background> </v-img>
+          <h1>This is an about page</h1>
+          <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque accusamus esse labore quisquam hic quaerat, maxime consequuntur eius, incidunt nihil veritatis voluptatem nulla provident similique, ipsum ut vitae tempora recusandae!</p>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,27 @@
   <div class="projects">
     
     <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+              <!-- <v-toolbar width="90%" class="mx-auto"> -->
+                <v-text-field
+                  clear-icon="mdi-close-circle"
+                  clearable
+
+                  single-line
+                  append-icon="mdi-magnify"
+                  solo
+                  class="mx-auto"
+                  label="Projects"
+                  @click:append="search"
+                  @input="filterInfo"
+                ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
       <v-row>
+        
         <v-col 
           cols="12"
           sm="6"
@@ -64,10 +84,14 @@ import Card from '@/components/Card.vue'
 
 export default {
   name: 'Projects',
+  created() {
+
+  },
   data () {
       return {
         dialog: false,
         id: null,
+        hasSearched: false,
         info: [
           {
             img:'Traffic.png',
@@ -96,28 +120,32 @@ export default {
             img:'Website.png',
             title:'Derivitive Trade Website',
             tags: ['Javascript','Python','Django','MongoDB'],
-            text: 'We created a website which accepts trades and stores them in a database. Using this we predict Derivitve trade info',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eleifend porta fermentum. Nullam bibendum at enim sed suscipit. Praesent accumsan elementum dui sit amet tincidunt. Sed non efficitur erat. Vivamus massa metus, tempus nec molestie ut, imperdiet sed turpis. Sed enim ex, malesuada congue elit ut, lacinia pharetra est. Maecenas tellus odio, ullamcorper vel luctus a, imperdiet fermentum erat. Etiam auctor cursus odio. Donec sed lectus vel arcu tincidunt rutrum. Fusce vel dui imperdiet, posuere ex ut, imperdiet sapien. Quisque ultricies fringilla blandit. Sed non enim iaculis ex commodo convallis. In dictum leo risus, vel sagittis neque placerat id. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque nisi nulla, pulvinar elementum gravida sed, rhoncus eu felis. Morbi congue lobortis quam nec malesuada. Aenean consequat, risus quis sodales consectetur, metus ligula pulvinar ipsum, vel venenatis mi est gravida sem. Nulla ornare euismod est, vitae laoreet velit laoreet quis. Cras semper ligula auctor mi condimentum luctus. Pellentesque eget elit at massa volutpat sagittis id eu leo. Etiam nisi ipsum, varius ac lacinia vitae, rhoncus nec nunc. Integer vel viverra lectus. Integer blandit blandit sem, congue sagittis ligula. Quisque nec nunc gravida, consectetur purus at, convallis ex. Aliquam ut sollicitudin erat. Quisque scelerisque luctus lorem id imperdiet. Maecenas dignissim vulputate erat, non blandit dolor elementum ut. Sed maximus porttitor luctus. Quisque nec leo ut elit posuere semper et ut risus. Pellentesque vel mollis dolor, id volutpat leo. Phasellus ut nisl interdum, rutrum magna at, elementum libero. Donec at tempus nisl. Duis leo enim, vehicula vel pretium ut, bibendum nec orci. Fusce aliquet lectus non urna pharetra, eu interdum felis pulvinar. Integer in eros neque.Etiam efficitur hendrerit odio, eu aliquet elit faucibus et. Fusce id risus elit. Sed auctor neque et dictum consequat. Proin quis ante ac elit viverra varius quis porta arcu. In ultricies ornare urna in fermentum. Suspendisse tincidunt quis risus quis imperdiet. Aenean et feugiat libero, ac vulputate metus. Aenean quis purus egestas, lobortis eros id, euismod dolor. Morbi ut malesuada lacus, facilisis congue neque. Quisque lacinia massa a egestas finibus. Cras purus ante, laoreet eget enim et, laoreet rhoncus sapien. Fusce efficitur ipsum a justo viverra consequat. Donec at rhoncus sapien.Ut eu dui eleifend, venenatis mi id, rutrum nunc. Nulla et varius nisl. Sed est sapien, bibendum id diam id, varius eleifend justo. Aliquam viverra consectetur felis sit amet consequat. Cras vitae consequat libero. Sed blandit ullamcorper mi, in ornare erat viverra id. Suspendisse potenti. Donec cursus augue non leo pharetra mollis ut id odio. Nam hendrerit nisl ex. Nunc ut lacus at urna porta semper. Sed at laoreet sem. Sed non est diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consec',
             index: 3
 
           },
-        ]        
+        ],    
       }
     },
     computed: {
-      numRows() {
-        // console.log(Math.ceil(this.info.length/2));
-        return Math.ceil(this.info.length/2)
-      }
+      
     },
 
     methods: {
       showCard(id) {
         this.dialog = true;
         this.id = id
-        console.log(this.id)
+        // console.log(this.id)
       },
       reached() {
         console.log("reached")
+      },
+      search() {
+        this.hasSearched = true;
+        console.log("search")
+      },
+      filterInfo(payload) {
+        console.log(payload)
       }
     },
   components: {
