ee9715b36f7f02c06eda805ec2e56860fdde368a
Iesa <iesa.wazeer7@gmail.com>
2020-08-02 22:19:08 +0530

    added some text in games page

--- frontend/src/views/Games.vue
+++ frontend/src/views/Games.vue
@@ -4,6 +4,7 @@
       <v-row>
         <v-col>
           <h1>This is a games page</h1>
+          <h3> Work in progress! Please come again later</h3>
         </v-col>
       </v-row>
     </v-container>
