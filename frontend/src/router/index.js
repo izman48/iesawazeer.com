d4edea901ac71dbb013a79e37d2809ca5d93f056
izman48 <iesa.wazeer7@gmail.com>
2021-02-11 16:09:57 +0000

    more about page removal

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,12 +9,12 @@ const routes = [{
         name: 'Home',
         component: Home,
     },
-    {
-        path: '/about',
-        name: 'About',
-        component: () =>
-            import ( /* webpackChunkName: "about" */ '../views/About.vue')
-    },
+    // {
+    //     path: '/about',
+    //     name: 'About',
+    //     component: () =>
+    //         import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    // },
     {
         path: '/games',
         name: 'Games',
